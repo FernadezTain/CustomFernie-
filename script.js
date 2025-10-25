0994be36206b74d1c29599d19c1c9b8cc6901867
FernadezTain <fernadezofficial489@gmail.com>
2025-10-26 01:14:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ const backgrounds = [
   // Платные фоны
   { file: "lizka_1.png", name: "Lizka", arg: "lizka_1", price: 17000, category: ["standard", "paid"] },
   { file: "lizka_2.png", name: "Lizka - 2", arg: "lizka_2", price: 30000, category: ["standard", "paid"] },
+  { file: "KO3AA_1.png", name: "KO3AA - 1", arg: "KO3AA_1", price: 10000, category: ["standard", "paid"] },
 ];
 
 const openBtn = document.getElementById("openBtn");
